0105dc31b260c0fbc58560af9a76fadcd84d08e0
Yashu Sachan <yashusachan440@gmail.com>
2025-08-22 23:21:37 +0000

    put this logo

--- src/components/icons/logo-icon.tsx
+++ src/components/icons/logo-icon.tsx
@@ -9,9 +9,11 @@ export function LogoIcon(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <g>
-        <path d="M0 25L50 0V50L0 75V25Z" fill="#FF0000" />
-        <path d="M50 0L100 25V75L50 100V50L50 0Z" fill="white" />
-        <path d="M0 75L50 100V50L0 75Z" fill="#FF0000" />
+        <path d="M50 2L2 26V75L50 99V51L50 2Z" fill="#FF0000"/>
+        <path d="M50 2L98 26V75L50 99V51L98 26" fill="white"/>
+        <path d="M50 2L2 26L50 51L98 26L50 2Z" fill="white"/>
+        <path d="M2 26L50 51V99L2 75V26Z" fill="#FF0000"/>
+        <path d="M98 26L50 51V99L98 75V26Z" fill="white"/>
       </g>
     </svg>
   );

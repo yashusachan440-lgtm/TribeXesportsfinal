3e440050238e8461f8219fa9064ae84c27d86ef2
Yashu Sachan <yashusachan440@gmail.com>
2025-08-22 23:34:27 +0000

    this logo

--- src/components/icons/logo-icon.tsx
+++ src/components/icons/logo-icon.tsx
@@ -11,15 +11,18 @@ export function LogoIcon(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-        <g>
-            <polygon points="50,10 20,30 20,70 50,90 80,70 80,30" fill="black" />
-            <polygon points="50,10 20,30 50,30" fill="#E50914"/>
-            <polygon points="50,10 80,30 50,30" fill="white"/>
-            <polygon points="20,30 20,55 50,70 50,30" fill="#E50914"/>
-            <polygon points="80,30 80,55 50,70 50,30" fill="white"/>
-            <polygon points="35,58 35,80 50,90 50,73" fill="#E50914"/>
-            <polygon points="65,58 65,80 50,90 50,73" fill="white"/>
-        </g>
+      <g>
+        <polygon points="50,15 20,30 20,70 50,85 80,70 80,30" fill="black" />
+        {/* Left Red Part */}
+        <polygon points="50,15 20,30 50,45" fill="#E50914"/>
+        <polygon points="20,30 20,70 50,85 50,45" fill="#E50914"/>
+
+        {/* Top White Part */}
+        <polygon points="50,15 80,30 50,45" fill="white"/>
+        
+        {/* Right White Part */}
+        <polygon points="80,30 80,70 50,85 50,45" fill="white"/>
+      </g>
     </svg>
   );
 }

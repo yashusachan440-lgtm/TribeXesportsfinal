1eda932b25102f13acd529517bfcf6ee1e320e98
Yashu Sachan <yashusachan440@gmail.com>
2025-08-22 23:35:05 +0000

    put exact logo

--- src/components/icons/logo-icon.tsx
+++ src/components/icons/logo-icon.tsx
@@ -5,24 +5,16 @@ export function LogoIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       width="100"
-      height="100"
-      viewBox="0 0 100 100"
-      fill="none"
+      height="115.47"
+      viewBox="0 0 100 115.47"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g>
-        <polygon points="50,15 20,30 20,70 50,85 80,70 80,30" fill="black" />
-        {/* Left Red Part */}
-        <polygon points="50,15 20,30 50,45" fill="#E50914"/>
-        <polygon points="20,30 20,70 50,85 50,45" fill="#E50914"/>
-
-        {/* Top White Part */}
-        <polygon points="50,15 80,30 50,45" fill="white"/>
-        
-        {/* Right White Part */}
-        <polygon points="80,30 80,70 50,85 50,45" fill="white"/>
-      </g>
+      <path d="M50 0L0 28.87v57.73L50 115.47l50-28.87V28.87L50 0z" fill="#1A1A1A"/>
+      <path d="M50 0L0 28.87v57.73L50 115.47z" fill="#E50914"/>
+      <path d="M50 0L100 28.87v57.73L50 115.47z" fill="#FFFFFF"/>
+      <path d="M50 57.73L0 28.87 50 0l50 28.87-50 28.86z" fill="#FFFFFF"/>
+      <path d="M50 57.73v57.74L0 86.6V28.87l50 28.86z" fill="#E50914"/>
     </svg>
   );
 }

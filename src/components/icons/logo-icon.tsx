7c81d64bff4d9c6ff39189ea2150f5d937373a41
Yashu Sachan <yashusachan440@gmail.com>
2025-08-22 23:22:24 +0000

    yet not exact logo even after giving svg

--- src/components/icons/logo-icon.tsx
+++ src/components/icons/logo-icon.tsx
@@ -4,17 +4,45 @@ import type { SVGProps } from "react";
 export function LogoIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 100 100"
+      width="100"
+      height="101"
+      viewBox="0 0 100 101"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g>
-        <path d="M50 2L2 26V75L50 99V51L50 2Z" fill="#FF0000"/>
-        <path d="M50 2L98 26V75L50 99V51L98 26" fill="white"/>
-        <path d="M50 2L2 26L50 51L98 26L50 2Z" fill="white"/>
-        <path d="M2 26L50 51V99L2 75V26Z" fill="#FF0000"/>
-        <path d="M98 26L50 51V99L98 75V26Z" fill="white"/>
+      <g clipPath="url(#clip0_2_61)">
+        <path
+          d="M49.9998 2.11914L1.99982 26.1191V75.1191L49.9998 99.1191V51.1191L49.9998 2.11914Z"
+          fill="#E50914"
+        />
+        <path
+          d="M49.9998 2.11914L97.9998 26.1191V75.1191L49.9998 99.1191V51.1191L97.9998 26.1191"
+          fill="white"
+        />
+        <path
+          d="M50 2.11914L2 26.1191L50 51.1191L98 26.1191L50 2.11914Z"
+          fill="#F5F5F1"
+        />
+        <path
+          d="M1.99982 26.1191L49.9998 51.1191V99.1191L1.99982 75.1191V26.1191Z"
+          fill="#B20710"
+        />
+        <path
+          d="M97.9998 26.1191L49.9998 51.1191V99.1191L97.9998 75.1191V26.1191Z"
+          fill="white"
+        />
       </g>
+      <defs>
+        <clipPath id="clip0_2_61">
+          <rect
+            width="96"
+            height="97"
+            fill="white"
+            transform="translate(2 2.11914)"
+          />
+        </clipPath>
+      </defs>
     </svg>
   );
 }

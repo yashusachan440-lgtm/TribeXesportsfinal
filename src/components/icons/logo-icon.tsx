eed3b4dd2935e5fc6366e92bb76d764d7736d85a
Yashu Sachan <yashusachan440@gmail.com>
2025-08-23 00:14:36 +0000

    use this picture as our logo

--- src/components/icons/logo-icon.tsx
+++ src/components/icons/logo-icon.tsx
@@ -5,16 +5,18 @@ export function LogoIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       width="100"
-      height="115.47"
-      viewBox="0 0 100 115.47"
+      height="100"
+      viewBox="0 0 100 100"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M50 0L0 28.87v57.73L50 115.47l50-28.87V28.87L50 0z" fill="#1A1A1A"/>
-      <path d="M50 0L0 28.87v57.73L50 115.47z" fill="#E50914"/>
-      <path d="M50 0L100 28.87v57.73L50 115.47z" fill="#FFFFFF"/>
-      <path d="M50 57.73L0 28.87 50 0l50 28.87-50 28.86z" fill="#FFFFFF"/>
-      <path d="M50 57.73v57.74L0 86.6V28.87l50 28.86z" fill="#E50914"/>
+      <path d="M0 0h100v100H0z" fill="#000"/>
+      <g fillRule="evenodd">
+        <path d="M50 15L25 27.5v25L50 65l25-12.5v-25L50 15zm0 3l22 11v22l-22 11-22-11v-22l22-11z" fill="#FFF"/>
+        <path d="M50 15L25 27.5v25L50 40v-25zM50 15L25 27.5 28 29v22l22-11V18l-3-1.5z" fill="red"/>
+        <path d="M25 55v35L50 80V55l-25 12.5z" fill="red"/>
+        <path d="M50 55v25l25 12.5V55L50 67.5z" fill="#FFF"/>
+      </g>
     </svg>
   );
 }

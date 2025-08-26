5a428862ff4bf92b1fb8ec2e08aa3f15301f9bc5
Yashu Sachan <yashusachan440@gmail.com>
2025-08-26 15:32:27 +0000

    put this svg as it is beside TribeX esports

--- src/components/icons/logo-icon.tsx
+++ src/components/icons/logo-icon.tsx
@@ -11,13 +11,13 @@ export function LogoIcon(props: SVGProps<SVGSVGElement>) {
       {...props}
     >
       <g>
-        <path d="M50 15 L35 22.5 L35 45 L50 37.5 Z" fill="red" />
-        <path d="M35 45 L35 77.5 L50 85 L50 52.5 Z" fill="red" />
-        <path d="M50 15 L65 22.5 L65 45 L50 37.5 Z" fill="white" />
-        <path d="M65 45 L65 77.5 L50 85 L50 52.5 Z" fill="white" />
+        <path d="M50 15 L35 22.5 L35 45 L50 37.5 Z" fill="#FF0000" />
+        <path d="M35 45 L35 77.5 L50 85 L50 52.5 Z" fill="#FF0000" />
+        <path d="M50 15 L65 22.5 L65 45 L50 37.5 Z" fill="#FFFFFF" />
+        <path d="M65 45 L65 77.5 L50 85 L50 52.5 Z" fill="#FFFFFF" />
         <path d="M50 15 L35 22.5 L50 30 L65 22.5 Z" fill="none">
-            <path d="M50 15 L35 22.5 L50 30 Z" fill="red" />
-            <path d="M50 15 L65 22.5 L50 30 Z" fill="white" />
+            <path d="M50 15 L35 22.5 L50 30 Z" fill="#FF0000" />
+            <path d="M50 15 L65 22.5 L50 30 Z" fill="#FFFFFF" />
         </path>
       </g>
     </svg>
